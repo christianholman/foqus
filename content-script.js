cc3f5390aa9b00c0282ab5a34dc53976ad7c76bf
Christian Holman <christian@holman.codes>
2022-12-25 20:39:39 +0000

    Initial commit

---
+++ content-script.js
@@ -0,0 +1,24 @@
+async function blockPage() {
+  fetch(chrome.runtime.getURL("blocked/blocked.html"))
+    .then((res) => res.text())
+    .then((text) => {
+      document.open();
+      document.write(text);
+      document.head.innerHTML += `<link rel="stylesheet" href="${chrome.runtime.getURL(
+        "blocked/blocked.css"
+      )}">`;
+      document.head.innerHTML += `<script src="${chrome.runtime.getURL(
+        "blocked/blocked.js"
+      )}"></script>`;
+      document.close();
+    });
+}
+
+chrome.storage.local.get(["blocked", "enabled"], ({ blocked, enabled }) => {
+  const res = window.location.hostname.match(/^(www\.|www\.)(.*)/);
+  const hostname = res ? res[res.length - 1] : window.location.hostname;
+  if (enabled && blocked[hostname]) {
+    window.stop();
+    blockPage();
+  }
+});
---
+++ popup.js
@@ -0,0 +1,57 @@
+const enabledCheckbox = document.getElementById("enabled-checkbox");
+const addWebsiteForm = document.getElementById("add-website-form");
+const websiteInput = document.getElementById("website-input");
+
+syncState();
+
+function updateBlockedSites() {
+  chrome.storage.local.get(["blocked"], ({ blocked }) => {
+    if (blocked) {
+      Object.keys(blocked).forEach((website) => {
+        const websiteNode = `<div class="blocked-site">${website}</div>`;
+        document.querySelector("#blocked-sites").innerHTML += websiteNode;
+      });
+    }
+  });
+}
+
+function updateEnabledcheckbox() {
+  chrome.storage.local.get(["enabled"], ({ enabled }) => {
+    console.log("enabled", enabled);
+    enabledCheckbox.checked = enabled;
+  });
+}
+
+function syncState() {
+  updateBlockedSites();
+  updateEnabledcheckbox();
+}
+
+addWebsiteForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (websiteInput.value) {
+    let url =
+      websiteInput.value.startsWith("http://") ||
+      websiteInput.value.startsWith("https://")
+        ? websiteInput.value
+        : "https://" + websiteInput.value;
+
+    const { hostname } = new URL(url);
+
+    if (hostname) {
+      chrome.storage.local.get(["blocked"], ({ blocked }) =>
+        chrome.storage.local.set({ blocked: { ...blocked, [hostname]: true } })
+      );
+    }
+
+    syncState();
+  }
+});
+
+enabledCheckbox.addEventListener("change", (event) => {
+  console.log("event", event);
+  chrome.storage.local.set({
+    enabled: enabledCheckbox.checked,
+  });
+  syncState();
+});
